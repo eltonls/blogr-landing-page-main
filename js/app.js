44c7f6ea4a90eb6ea534f6e5efc84034961eac26
Elton <eltonnrs25@gmail.com>
2021-04-11 10:29:05 -0300

    Added Button functionality
    Added button functionality and js file along with some menu styling and media queries

---
+++ js/app.js
@@ -0,0 +1,27 @@
+const menuBtns = Array.from(document.querySelectorAll(".nav__btn"));
+const subMenuList = Array.from(document.querySelectorAll(".nav__menu--sub"));
+const hamburguerBtn = document.querySelector(".hamburguer");
+const nav = document.querySelector(".nav");
+menuBtns.forEach((element, idx) => {
+    element.addEventListener("click", function(){
+        if(subMenuList[idx].classList[1] === "active"){
+            subMenuList[idx].classList.remove("active");
+        }
+        else{
+            subMenuList.forEach(element => {
+                element.classList.remove("active");
+            });
+            subMenuList[idx].classList.add("active");
+        }
+    });
+});
+hamburguerBtn.addEventListener("click", function(){
+    nav.classList.toggle("active-nav");
+    if(nav.classList[1] === "active-nav"){
+        hamburguerBtn.children[0].setAttribute("src", "../images/icon-close.svg");
+    }
+    else{
+        hamburguerBtn.children[0].setAttribute("src", "../images/icon-hamburger.svg");
+    }
+});
+
